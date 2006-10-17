bf5bafc7d9f30ce48908a1bd3409419db681bdf4
ondrej.certik <ondrej.certik@b93ee803-ef1f-0410-b89a-8f43c5ceceb5>
2006-10-17 11:32:07 +0000

    doc update

--- poly/poly.py
+++ poly/poly.py
@@ -11,6 +11,10 @@ Usage:
 >>> poly.triangulate([(0,0), (1,0), (0,1), (0.5, 0.25)])
 [[(1, 0), (0, 1), (0.5, 0.25)], [(0, 0), (1, 0), (0.5, 0.25)]]
 
+
+Note: This algorithm is a home-made hybrid, better is to use "ear clipping"
+algorithm, but I didn't know about it when I wrote this.
+
 """
 
 def isright(a,b,c):
