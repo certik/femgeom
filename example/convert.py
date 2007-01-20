775a9a66ad867a5fe58b13be6a3beb4ce6bf72f0
ondrej.certik <ondrej.certik@b93ee803-ef1f-0410-b89a-8f43c5ceceb5>
2007-01-20 20:17:46 +0000

    make tetgen write region atributes

--- example/convert.py
+++ example/convert.py
@@ -9,11 +9,22 @@ import pexpect
 import geom
 from geom.meshutils import mesh
 
+def tetgen(tetgenpath,filename):
+    cmd="%s -pqQA -a0.01 %s"%(tetgenpath,filename)
+    p=pexpect.spawn(cmd)
+    p.expect("Opening %s."%(filename))
+    assert p.before==""
+    p.expect(pexpect.EOF)
+    if p.before!="\r\n":
+        print p.before
+        raise "Error when running tetgen (see above for output): %s"%cmd
+
+
 pexpect.run("gmsh -0 t.geo -o /tmp/x.geo")
 g=geom.read_gmsh("/tmp/x.geo")
 g.printinfo()
 geom.write_tetgen(g,"/tmp/t.smesh")
-pexpect.run("/home/ondra/fzu/mesh/tetgen -pq -a0.01 /tmp/t.smesh")
+tetgen("/home/ondra/fzu/mesh/tetgen","/tmp/t.smesh")
 
 m=mesh()
 geom.read_tetgen(m,"/tmp/t.1")
--- geom/geometry.py
+++ geom/geometry.py
@@ -1,9 +1,23 @@
 class geometry(object):
     """The geometry is given by a sets of points (d0), lines (d1), surfaces
-    (d2) and volumes (d3). Lines are constructed from 2 points, surface from
-    any number of lines, volume from any number of surfaces.
+    (d2) and volumes (d3). A lines are constructed from 2 points, a surface from
+    any number of lines, a volume from any number of surfaces.
 
     Physical volumes are contruted from any number of volumes.
+
+
+    The self.d0, self.d1, self.d2 and self.d3 are dictionaries holding a map
+
+    geometry element number  ->   instance of point,line,surface of volume
+
+    Example:
+    ========
+
+    To get all the points which define a surface 5, use:
+
+    self.d2[5].getpoints()
+
+    This would give you a list [..] of point() instances.
     """
     def __init__(self):
         self.d0={}
@@ -15,25 +29,36 @@ class geometry(object):
         "p=[x,y,z]"
         o=point(self,n,p)
         self.d0[o.getn()]=o
-    def addline(self,n,l)
+    def addline(self,n,l):
         "l=[p1,p2]"
         o=line(self,n,l)
         self.d1[o.getn()]=o
+    def addsurface(self,n,s):
+        "s=[l1,l2,l3,...]"
+        o=surface(self,n,s)
+        self.d2[o.getn()]=o
+    def addvolume(self,n,v):
+        "v=[s1,s2,s3,...]"
+        o=volume(self,n,v)
+        self.d3[o.getn()]=o
+    def addphysicalvolume(self,n,volumelist):
+        "volumelist=[v1,v2,v3,...]"
+        o=physicalvolume(self,n,volumelist)
+        self.phys3[o.getn()]=o
     def printinfo(self):
         print "General geometry information:"
         print "  points:",len(self.d0)
         print "  lines:",len(self.d1)
         print "  surfaces:",len(self.d2)
         print "  volumes:",len(self.d3)
-        print "Physical volumes:"
-        for d in self.phys3:
-            print d,self.phys3[d]
+        print "Physical entities:"
+        print "  volumes:"
+        for d in self.phys3.values():
+            print "    %d: %r"%(d.getn(),d.volumes)
 
 class geomobject(object):
     def getn(self):
-        return self.data[0]
-    def __repr__(self):
-        return str(type(self))[18]+repr(self.data[1:])
+        return self.n
 
 class point(geomobject):
     def __init__(self,g,n,p):
@@ -42,8 +67,6 @@ class point(geomobject):
         self.p=p
     def getxyz(self):
         return self.p
-    def getn(self):
-        return self.n
     def getstr(self):
         return "%f, %f, %f"%self.getxyz()
 
@@ -53,81 +76,37 @@ class line(geomobject):
         self.n=n
         self.points=l
     def getpoints(self):
-        return self.points
-#    def reverse(self):
-#        l= line(-self.n,self.points[1],self.points[0])
-#        l.geom=self.geom
-#        return l
-
-class lineloop(geomobject):
-    def __init__(self,n,lines):
-        self.data=(n,lines)
-    def getlines(self):
-        s=[]
-        for x in self.data[1]:
-            if x>0: s.append(self.geom.d1[x])
-            else: s.append(self.geom.d1[-x].reverse())
-        return s
-
-class circle(geomobject):
-    def __init__(self,n,p1,p2,p3):
-        self.data=(n,)
-        self.n=n
-        self.points=p1,p2,p3
-    def getpoints(self):
         return [self.geom.d0[x] for x in self.points]
-    def reverse(self):
-        c= circle(-self.n,self.points[2],self.points[1],self.points[0])
-        c.geom=self.geom
-        return c
 
-class planesurface(geomobject):
-    def __init__(self,n,boundary):
-        self.data=(n,)
-        self.boundary=boundary
-    def getlines(self):
-        #this doesn't work for negative
-        return self.geom.d1[self.boundary].getlines()
+class surface(geomobject):
+    def __init__(self,g,n,s):
+        self.geom=g
+        self.n=n
+        self.lines=s
     def getpoints(self):
-        lines=self.getlines()
-        return [x.getpoints()[0] for x in lines]
-
-class ruledsurface(geomobject):
-    def __init__(self,n,boundary):
-        self.data=(n,)
-        self.boundary=boundary
-    def getlines(self):
-        #this doesn't work for negative
-        return self.geom.d1[self.boundary].getlines()
-    def getpoints(self):
-        lines=self.getlines()
-        return [x.getpoints()[0] for x in lines]
-
-class surfaceloop(geomobject):
-    def __init__(self,n,surfaces):
-        self.data=(n,surfaces)
-        self.surfaces=surfaces
-    def getsurfaces(self):
-        s=[]
-        for x in self.surfaces:
-            if x>0: s.append(self.geom.d2[x])
-            else: s.append(self.geom.d2[-x]) #we should actually reverse it
-        return s
+        #self.lines contains the numbers of all the lines
+        def p(idx):
+            "Return the correct point of the line 'idx'"
+            if idx>0:
+                return self.geom.d1[idx].getpoints()[0]
+            else:
+                return self.geom.d1[-idx].getpoints()[1]
+        return [p(x) for x in self.lines]
 
 class volume(geomobject):
-    def __init__(self,n,boundary):
-        self.data=(n,boundary)
-        self.boundary=boundary
+    def __init__(self,g,n,v):
+        self.geom=g
+        self.n=n
+        self.surfaces=v
     def getsurfaces(self):
-        return self.geom.d2[self.boundary].getsurfaces()
+        return [self.geom.d2[x] for x in self.surfaces]
+    def getinsidepoint(self):
+        return self.getsurfaces()[0].getpoints()[0].getxyz()
 
-class physvolume(geomobject):
-    def __init__(self,n,volumes):
-        self.data=(n,volumes)
-        self.volumes=volumes
+class physicalvolume(geomobject):
+    def __init__(self,g,n,v):
+        self.geom=g
+        self.n=n
+        self.volumes=v
     def getvolumes(self):
-        s=[]
-        for x in self.volumes:
-            if x>0: s.append(self.geom.d3[x])
-            else: raise "negative volume ID"
-        return s
+        return [self.geom.d3[x] for x in self.volumes]
