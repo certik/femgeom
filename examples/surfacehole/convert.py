bc77f2e8bb7cdc2fe7cd4cd22405f16227835807
ondrej.certik <ondrej.certik@b93ee803-ef1f-0410-b89a-8f43c5ceceb5>
2007-01-25 13:44:31 +0000

    examples fixed

--- examples/surfacehole/convert.py
+++ examples/surfacehole/convert.py
@@ -1,26 +1,11 @@
 #! /usr/bin/env python
 
-#import cgitb;cgitb.enable(format="text")
-
 import sys
 sys.path.append("../..")
-import os
 
 import pexpect
 
 import geom
-
-def tetgen(tetgenpath,filename):
-    #cmd="%s -pqQA -a0.01 %s"%(tetgenpath,filename)
-    cmd="%s -pqQA %s"%(tetgenpath,filename)
-    p=pexpect.spawn(cmd)
-    p.expect("Opening %s."%(filename))
-    assert p.before==""
-    p.expect(pexpect.EOF)
-    if p.before!="\r\n":
-        print p.before
-        raise "Error when running tetgen (see above for output): %s"%cmd
-
 
 pexpect.run("gmsh -0 t.geo -o /tmp/x.geo")
 g=geom.read_gmsh("/tmp/x.geo")
@@ -28,10 +13,10 @@ g.printinfo()
 geom.write_tetgen(g,"/tmp/t.poly")
 
 print "Generating mesh using tetgen..."
-tetgen("/home/ondra/fzu/mesh/tetgen","/tmp/t.poly")
+geom.runtetgen("/home/ondra/fzu/mesh/tetgen","/tmp/t.poly")
 
 print "Reading mesh from tetgen..."
-m,b=geom.read_tetgen("/tmp/t.1")
+m=geom.read_tetgen("/tmp/t.1")
 m.writemsh("/tmp/t12.msh")
 print "Mesh written to /tmp/t12.msh"
 m.writexda("/tmp/in.xda")
